76a11e6b659bb19cf519f4006ed60122b7194f3e
Masna1994 <39308574+Masna1994@users.noreply.github.com>
2018-05-16 08:29:52 +0700

    Add files via upload

---
+++ new.php
@@ -0,0 +1,9 @@
+<!doctype html>  
+     <html>  
+     <head>  
+     <meta charset="utf-8">   
+     </head>  
+     <body>  
+     <?php echo "nama : Masna Nur Hasan NIM: 175410211"; ?>  
+     </body>  
+     </html> 
